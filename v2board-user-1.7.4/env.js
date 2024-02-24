d8fb2ef7ea60d0044a5a02161f808a51e654ee7e
dirhana <113638840+dirhana@users.noreply.github.com>
2024-02-25 04:43:27 +0800

    Add files via upload

---
+++ v2board-user-1.7.4/env.js
@@ -0,0 +1,27 @@
+window.settings = {
+  // 站点标题
+  title: '樱花云',
+  // 站点描述
+  description: '访问ChatGPT，解锁流媒体，安全匿名加速服务',
+  // API
+  host: 'http://38.207.175.54:12580',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: 'https://www.hostpic.org/images/2401032021260103.jpeg',
+  // crisp
+  crisp_id: '5d7be2dc-fec3-44a5-a1e9-87919e6d48d6',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
