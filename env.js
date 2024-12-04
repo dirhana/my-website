067040da7fc6de400ef4ea68bf416b8ad24a0823
dirhana <113638840+dirhana@users.noreply.github.com>
2024-12-04 17:41:11 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,27 @@
+window.settings = {
+  // 站点标题
+  title: '樱花云☁️',
+  // 站点描述
+  description: '高速 | 匿名 | 稳定 | 实惠 | 保障',
+  // API
+  host: 'https://1324398847-1vckz3zmzj-hk.scf.tencentcs.com',
+  // 主题
+  theme: {
+    sidebar: 'dark',
+    header: 'dark',
+    color: 'darkblue'
+  },
+  // 背景 https://www.hostpic.org/images/2401032021260103.jpeg
+  background_url: 'https://img.meituan.net/video/dceb5e50e20ca54dd186dc82ecf2987d61135.jpg',
+  // crisp
+  crisp_id: '5d7be2dc-fec3-44a5-a1e9-87919e6d48d6',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
